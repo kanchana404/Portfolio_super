a767dffe9c97929f7d00c048a9c382faf57b96aa
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-18 01:05:08 +0530

    done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,12 +27,12 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web magiv with Next.js
+           Let's Make Something Great
           </h2>
 
           <TextGenerateEffect 
           className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          words="Transforming Concepts into Seamless Experiences."
+          words="Transforming Concepts into Seamless User Experiences."
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
