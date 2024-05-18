2bd3946939be53eb3e311b23bb090848d59dd920
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-18 14:07:32 +0530

    www

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
     <div
       className={cn(
           // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
-          "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
