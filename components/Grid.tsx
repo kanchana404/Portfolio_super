035cd5c1407177282de12d1ab2982626cfa3b922
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-18 12:16:23 +0530

    grid anabled

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,28 +1,22 @@
-import { gridItems } from "@/data";
-import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section id="about">
-      <BentoGrid className="w-full py-20">
-        {gridItems.map((item) => (
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map((item, index) => (
           <BentoGridItem
-            id={item.id}
-            key={item.id}
+           id={item.id}
+           key={item.id}
             title={item.title}
             description={item.description}
-
-            className={item.className}
-            img={item.img}
-            imgClassName={item.imgClassName}
-            titleClassName={item.titleClassName}
-            spareImg={item.spareImg}
-
           />
         ))}
       </BentoGrid>
     </section>
-  );
-};
+  )
+}
 
-export default Grid;
+export default Grid
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -21,39 +21,29 @@ export const BentoGrid = ({
 
 export const BentoGridItem = ({
   className,
-  id,
   title,
   description,
-  img,
-  imgClassName,
-  titleClassName,
-  spareImg,
+  header,
+  icon,
+  id,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id?: number;
-  img?: string;
-  imgClassName?: string;
-  titleClassName?: string;
-  spareImg: string;
+  id:number;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-xl group/bento hover:shadow-3xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4  ",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
         className
       )}
-      style={{
-        background: 'rgb(4,7,29)',
-        backgroundColor: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-      }}
     >
-     
+      {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        
+        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
