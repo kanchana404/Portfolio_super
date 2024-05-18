97afe6b9d2a1a12be88ca8304941f039b0e6f005
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-19 01:17:50 +0530

    duh

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "Tech enthusiast with a passion for mainly web development.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
