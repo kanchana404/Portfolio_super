947bfdb22eb89e9cfdc90fa68bef2380329b498d
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-20 01:14:55 +0530

    awfsd

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -72,10 +72,7 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        
       </motion.div>
     </AnimatePresence>
   );
