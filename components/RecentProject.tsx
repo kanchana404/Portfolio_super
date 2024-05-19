107266676ff1d9d2d0ec27403c5e9496570ec2f7
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-19 13:01:49 +0530

    awdasfa

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -1,38 +1,58 @@
-import { projects } from "@/data";
-import React from "react";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
-import { PinContainer } from "./ui/3d-pin";
-import { FaLocationArrow } from "react-icons/fa";
+"use client";
 
-const RecentProject = () => {
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { projects } from "@/data";
+import { PinContainer } from "./ui/3d-pin";
+
+const RecentProjects = () => {
   return (
     <div className="py-20">
       <h1 className="heading">
-        A small selection of {""}
+        A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
-        {projects.map(({ id, title, des, img, iconLists, link }) => (
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map((item) => (
           <div
-            key={id}
-            className="sm:h-[41rem] h-[32rem]lg:min-h[35.5rem] flex items-center justify-center w-[80vh] sm:w-[570px]"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={item.id}
           >
-            <PinContainer title={title} href={link}>
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] sm:h-[40vh] overflow-hidden h-[30vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                  <img src="/bg.png" alt="bg_img" />
+            <PinContainer
+              title="/ui.aceternity.com"
+              href="https://twitter.com/mannupaaji"
+            >
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img src="/bg.png" alt="bgimg" />
                 </div>
-                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute bottom-0"
+                />
               </div>
+
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {title}
+                {item.title}
               </h1>
-              <p className="lg:text-xl lg;font-normal font-light text-sm line-clamp-2">
-                {des}
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {item.des}
               </p>
+
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {iconLists.map((icon, index) => (
+                  {item.iconLists.map((icon, index) => (
                     <div
                       key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
@@ -44,9 +64,12 @@ const RecentProject = () => {
                     </div>
                   ))}
                 </div>
+
                 <div className="flex justify-center items-center">
-                <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check live site</p>
-                <FaLocationArrow className="ms-3" color="#CBACF9"/>
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                   <a href="">Check Live Site</a> 
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
             </PinContainer>
@@ -57,4 +80,4 @@ const RecentProject = () => {
   );
 };
 
-export default RecentProject;
+export default RecentProjects;
