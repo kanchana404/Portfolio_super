2a92d0e4bb09ca48b15c97327b6dc8e22ab3d4a3
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-19 11:50:08 +0530

    bentoGrid done

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently Learning and mastering NEXT.js",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
