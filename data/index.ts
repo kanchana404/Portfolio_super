b55da81efb6618f918f3d9306145e39b4cd09db9
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-19 11:48:10 +0530

    awdadw

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "I prioritize client collaboration, fostering open communication. Open to work any time ",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
