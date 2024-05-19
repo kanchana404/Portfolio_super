69e40af0561736fd5d1c5c42729d79398423bca3
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-05-19 20:45:07 +0530

    company done

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
-import { testimonials } from '@/data'
+import { companies, testimonials } from '@/data'
 
 const Clients = () => {
   return (
@@ -16,6 +16,15 @@ const Clients = () => {
          direction='right'
          speed='slow'
          />
+
+         <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+          {companies.map(({id, img, name, nameImg}) => (
+            <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+              <img src={img} alt={name} className='md:w-10 w-5' />
+              <img src={nameImg} alt={name} className='md:w-24 w-20' />
+            </div>
+          ))}
+         </div>
         </div>
         
     </div>
