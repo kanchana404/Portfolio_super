6d426299c6c4f804d7e1974d065119ca7591fccd
Kavitha Kanchana <146015143+kanchana404@users.noreply.github.com>
2024-07-21 03:03:27 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProject />
-        <Clients />
+{/*         <Clients /> */}
         <Expirience />
         <Approch />
         <Footer />
